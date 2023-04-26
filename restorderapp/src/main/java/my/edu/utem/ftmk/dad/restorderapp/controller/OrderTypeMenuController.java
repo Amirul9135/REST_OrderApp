a64ea3bfd4dca85237b7174c51cbe297542e3123
Amirul9135 <amirul.asraf9135@gmail.com>
2023-04-26 20:43:36 +0800

    ordertype update

---
+++ restorderapp/src/main/java/my/edu/utem/ftmk/dad/restorderapp/controller/OrderTypeMenuController.java
@@ -0,0 +1,123 @@
+package my.edu.utem.ftmk.dad.restorderapp.controller;
+ 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import my.edu.utem.ftmk.dad.restorderapp.model.OrderType;
+
+
+/*
+ *This controller class contain methods that consume the webservice from OrderTypeRESTController  
+ */
+@Controller
+public class OrderTypeMenuController {
+	
+	private String defaultURI = "http://localhost:8080/orderapp/api/ordertypes";
+	
+	
+	/*
+	 * This method save changes made to ordertype
+	 */
+	@RequestMapping("/ordertype/save")
+	public String updateOrderType(@ModelAttribute OrderType orderType) {
+		// Create new Rest Template
+		RestTemplate restTemplate = new RestTemplate();
+		
+		//Create request body
+		HttpEntity<OrderType> requestEntity = new HttpEntity<OrderType>(orderType);
+		
+		String orderTypResponse ="";
+		if(orderType.getOrderTypeId() > 0) {
+			//if id>0 means existing so update 
+			//consume the service in REST controller
+			restTemplate.put(defaultURI, requestEntity,OrderType.class);
+		}
+		else {
+			//if id <=0 means no id yet, new ordertype
+			orderTypResponse = restTemplate.postForObject(defaultURI, requestEntity, String.class);
+		}
+		
+		System.out.println(orderTypResponse);
+		
+		//redirect user to ordertype/list
+		return "redirect:/ordertype/list";
+	}
+	
+	
+	/*
+	 * This method return a web page consisting of order type list
+	 * when user access orderapp/ordertype/list via browser
+	 */
+	@GetMapping("/ordertype/list")
+	public String getOrderTypes(Model model) {
+		//uri to access the ordertype web service 
+				
+		//request the data from the web service
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<OrderType[]> response = restTemplate.getForEntity(defaultURI, OrderType[].class);
+				
+		//conversion of response from web service to java object array
+		OrderType orderTypes[] = response.getBody();
+		
+		//parse array to list
+		List<OrderType> orderTypeList = Arrays.asList(orderTypes);
+		
+		//attach list to model as attribute
+		model.addAttribute("orderTypes",orderTypeList);
+		
+		//return the name of template(html) to be used within the template folder
+		//start from templates, if in folder in template use foldername/filename n so on
+		return "ordertypes";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/* OLD VERSION
+	 * This method handle the request to /orderapp/ordertype/list
+	 * this method will print the list of ordertype in the console
+	 
+	@GetMapping("/ordertype/list")
+	public ResponseEntity<String> getOrderTypes() {
+		
+		//uri to access the ordertype web service
+		String uri = "http://localhost:8080/orderapp/api/ordertypes";
+		
+		//request the data from the web service
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<OrderType[]> response = restTemplate.getForEntity(uri, OrderType[].class);
+		
+		//conversion of response from web service to java object array
+		OrderType orderTypes[] = response.getBody();
+		
+		//Display the data into console
+		System.out.println(this.getClass().getSimpleName());
+		System.out.println("Total records: " + orderTypes.length + "\n");
+		
+		for(OrderType orderType:orderTypes) {
+			System.out.print(orderType.getOrderTypeId() + "-");
+			System.out.print(orderType.getCode() + "-");
+			System.out.println(orderType.getName());
+		}
+		
+		//return a string as response message
+		String messageString = "Chech out message in the console";
+		return new ResponseEntity<>(messageString, HttpStatus.OK);
+	}
+	*/
+}
